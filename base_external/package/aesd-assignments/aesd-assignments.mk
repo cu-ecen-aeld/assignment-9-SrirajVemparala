562af08c319aff3a22d58bd012e62ceffbcf5864
vsrlinux <vsrlinux@vsrlinux.myguest.virtualbox.org>
2024-02-29 20:18:07 -0700

    Changed the commit

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION ='787fa6099f1fa0a6df8a7e0e20264bd4910b85c2'
+AESD_ASSIGNMENTS_VERSION ='57fe2e48bc781933dd75db1224f5eaa4f0b83ba2'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
