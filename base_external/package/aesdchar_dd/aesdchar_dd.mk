7749d20bf6e8381b861a05ec987d04203e4e3e4c
Raghu Vemparala <rave4408@aesd-students.colorado.edu>
2024-04-02 03:02:34 +0000

    Added_A9_changes

--- base_external/package/aesdchar_dd/aesdchar_dd.mk
+++ base_external/package/aesdchar_dd/aesdchar_dd.mk
@@ -5,7 +5,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESDCHAR_DD_VERSION ='05db8be89e73392b9d5516e03d5c872f716058bb'
+AESDCHAR_DD_VERSION ='d89f06ed7ca808e55b428ac8aafc6cc3edb911be'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
